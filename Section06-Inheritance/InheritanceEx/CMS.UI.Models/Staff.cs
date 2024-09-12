db26abb0094e5a968cd92473b3aaae72ba86c9ac
arewezaetoime <hristoiliev.1994@gmail.com>
2024-09-12 22:52:56 +0300

    asa

---
+++ Section06-Inheritance/InheritanceEx/CMS.UI.Models/Staff.cs
@@ -0,0 +1,37 @@
+namespace CMS.UI.Models
+{
+    public class Staff : Person
+    {
+        private string firstName;
+
+        public int Id { get; private set; }
+        public int WorkingHoursPerWeek { get; set; }
+
+        public Staff(string firstName, string LastName) : base(firstName, LastName)
+        {
+            FirstName = firstName;
+            this.LastName = LastName;
+            WorkingHoursPerWeek = 40;
+        }
+      
+
+        protected void CalculateFees(decimal electiveFees, ref decimal roughFees,
+            out decimal finalFees)
+            {
+                electiveFees = 10000;
+                roughFees = 10000;
+                finalFees = 10000;
+            }
+        
+        // public void UpdateInfo(string firstName)
+        // {
+        //     FirstName = firstName;
+        // }
+
+        protected internal void UpdateInfo(string firstName, string lastName = "")
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
