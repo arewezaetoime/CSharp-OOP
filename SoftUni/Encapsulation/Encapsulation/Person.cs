d74cd330235940472089f3cee8630948be63dc0b
arewezaetoime <hristoiliev.1994@gmail.com>
2024-09-15 10:35:01 +0300

    encap

---
+++ SoftUni/Encapsulation/Encapsulation/Person.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Encapsulation
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+    }
+}
---
+++ SoftUni/Encapsulation/Encapsulation/StartUp .cs
@@ -0,0 +1,25 @@
+﻿using Encapsulation;
+
+namespace PersonsInfo
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split(' ');
+                Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                persons.Add(person);
+            }
+
+            foreach (var p in persons.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList())
+            {
+                Console.WriteLine(p.ToString());
+            };
+        }
+    }
+}
