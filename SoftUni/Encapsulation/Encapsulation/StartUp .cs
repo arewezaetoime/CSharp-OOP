aeb6d1ebf99c98993120cd0e77c15055a9b9bf37
arewezaetoime <hristoiliev.1994@gmail.com>
2024-09-16 12:23:32 +0300

    arewewewe

--- SoftUni/Encapsulation/Encapsulation/StartUp .cs
+++ SoftUni/Encapsulation/Encapsulation/StartUp .cs
@@ -2,41 +2,51 @@
 
 namespace PersonsInfo
 {
-    internal class StartUp
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            /* var lines = int.Parse(Console.ReadLine());
-             var persons = new List<Person>();
-
-             for (int i = 0; i < lines; i++)
-             {
-                 var cmdArgs = Console.ReadLine().Split(' ');
-                 Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
-                 persons.Add(person);
-             }
-
-             foreach (var p in persons.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList())
-             {
-                 Console.WriteLine(p.ToString());*/
-
-
             var lines = int.Parse(Console.ReadLine());
-            var persons = new List<Person>();
+            //var persons = new List<Person>();
+
+            var team = new Team("Arewe");
+
             for (int i = 0; i < lines; i++)
             {
-                var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0],
-                                        cmdArgs[1],
-                                        int.Parse(cmdArgs[2]),
-                                        decimal.Parse(cmdArgs[3]));
-
-                persons.Add(person);
+                var cmdArgs = Console.ReadLine().Split(' ');
+                Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+                team.AddPlayer(person);
             }
-            var parcentage = decimal.Parse(Console.ReadLine());
-            persons.ForEach(p => p.IncreaseSalary(parcentage));
-            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Console.WriteLine(team.FirstTeam.Count());
+            Console.WriteLine(team.ReserveTeam.Count());
+
+
+
+            //foreach (var p in persons.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList())
+            //{
+            //    Console.WriteLine(p.ToString());
+
+
+            //    var lines = int.Parse(Console.ReadLine());
+            //    var persons = new List<Person>();
+            //    for (int i = 0; i < lines; i++)
+            //    {
+            //        var cmdArgs = Console.ReadLine().Split();
+            //        var person = new Person(cmdArgs[0],
+            //                                cmdArgs[1],
+            //                                int.Parse(cmdArgs[2]),
+            //                                decimal.Parse(cmdArgs[3]));
+
+            //        persons.Add(person);
+            //    }
+            //    var parcentage = decimal.Parse(Console.ReadLine());
+            //    persons.ForEach(p => p.IncreaseSalary(parcentage));
+            //    persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+
+
 
         }
-    }
+        }
 }
---
+++ SoftUni/Encapsulation/Encapsulation/Team.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Encapsulation
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public string Name { get; private set; }
+        public IReadOnlyCollection<Person> FirstTeam => firstTeam.AsReadOnly();
+        public IReadOnlyCollection<Person> ReserveTeam => reserveTeam.AsReadOnly();
+
+        public void AddPlayer(Person p)
+        {
+            if (p.Age < 40)
+            {
+                firstTeam.Add(p);
+            }
+            else
+            {
+                reserveTeam.Add(p); 
+            }
+        }
+    }
+
+}
