da05f1e5b71995756bff56fa7f9f334e85209d41
arewezaetoime <hristoiliev.1994@gmail.com>
2024-09-16 11:17:12 +0300

    awe

--- SoftUni/Encapsulation/Encapsulation/Person.cs
+++ SoftUni/Encapsulation/Encapsulation/Person.cs
@@ -6,21 +6,95 @@ namespace Encapsulation
         private string firstName;
         private string lastName;
         private int age;
+        private decimal salary;
 
-        public Person(string firstName, string lastName, int age)
+        public Person(string firstName, string lastName, int age, decimal salary)
         {
             FirstName = firstName;
             LastName = lastName;
             Age = age;
+            Salary = salary;
         }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value < 650m)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                else
+                {
+                    salary = value;
+                }
+
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage *= 0.5m;
+            }
+
+            this.Salary *= 1 + (percentage / 100);
+        }
 
         public override string ToString()
         {
-            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
         }
     }
 }
