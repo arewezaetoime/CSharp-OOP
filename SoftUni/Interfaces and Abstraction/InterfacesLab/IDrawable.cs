be019aa2d0817c2fefe2e549928d5795fef1ef1e
arewezaetoime <hristoiliev.1994@gmail.com>
2024-09-17 22:19:39 +0300

    adsa

--- SoftUni/Interfaces and Abstraction/InterfacesLab/IDrawable.cs
+++ SoftUni/Interfaces and Abstraction/InterfacesLab/IDrawable.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace InterfacesLab
 {
-    internal interface Interface1
+    internal interface IDrawable
     {
+        void Draw();
     }
 }
--- SoftUni/Interfaces and Abstraction/InterfacesLab/Rectangle.cs
+++ SoftUni/Interfaces and Abstraction/InterfacesLab/Rectangle.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace InterfacesLab
 {
-    internal class Rectangle
+    internal class Rectangle : IDrawable
     {
+
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+            DrawLine(this.width, '*', '*');
+        }
+
+        public void DrawLine(int width, char end, char mid)
+        {
+            Console.WriteLine(end);
+            for (int i = 0; i < width - 1; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
     }
 }
