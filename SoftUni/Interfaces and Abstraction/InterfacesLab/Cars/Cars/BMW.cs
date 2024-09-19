66594c46578d747210966b4c9993a0b2e555cd2b
arewezaetoime <hristoiliev.1994@gmail.com>
2024-09-19 22:57:46 +0300

    asdas

---
+++ SoftUni/Interfaces and Abstraction/InterfacesLab/Cars/Cars/BMW.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Cars
+{
+    internal class BMW : ICar
+    {
+
+        public string Model { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Color { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public BMW(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} BMW {this.Model}";
+        }
+    }
+}
---
+++ SoftUni/Interfaces and Abstraction/InterfacesLab/Cars/Cars/Tesla.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Cars
+{
+    internal class Tesla : ICar, IElectricCar
+    {
+        public int Battery { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Model { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Color { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Tesla(int battery, string model, string color)
+        {
+            Battery = battery;
+            Model = model;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla Model {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}
