057f16948136e02a5c58f64892e1886bf5cf5c49
arewezaetoime <hristoiliev.1994@gmail.com>
2024-09-20 22:55:30 +0300

    arewe

---
+++ SoftUni/Polymorphism/Polymorphism/Task2-Override/Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism.Task2_Override
+{
+    internal class Cat
+    {
+    }
+}
