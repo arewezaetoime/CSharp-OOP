41e79a21f9b2d824a0e8a9b43cd8df785e038ff5
arewezaetoime <hristoiliev.1994@gmail.com>
2024-09-22 12:05:57 +0300

    asa

--- SoftUni/Polymorphism/Polymorphism/Program.cs
+++ SoftUni/Polymorphism/Polymorphism/Program.cs
@@ -1,13 +1,24 @@
-﻿namespace Polymorphism
+﻿
+using Animals;
+
+namespace Polymorphism
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            MathOperations mo = new MathOperations();
-            Console.WriteLine(mo.Add(2, 3));
-            Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
-            Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
+            //MathOperations mo = new MathOperations();
+            //Console.WriteLine(mo.Add(2, 3));
+            //Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
+            //Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
+
+
+            Animal cat = new Cat("Peter", "Whiskas");
+            //Animal dog = new Dog("George", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            //Console.WriteLine(dog.ExplainSelf());
+
         }
     }
 }
